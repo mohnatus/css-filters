73a7000ba0f5e57b37d8af7edb394b7d3cfe2c9c
FurryCat <furrycat.web@gmail.com>
2020-04-09 20:22:42 +0300

    limit img size

--- src/components/Image/Images.js
+++ src/components/Image/Images.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles(({ breakpoints }) => ({
+  img: {
+    maxHeight: 300,
+    maxWidth: 450,
+    width: '100%',
+  },
+  wrapper: {
+    textAlign: 'center',
+    [breakpoints.up('md')]: {
+      '&:first-child': {
+        textAlign: 'right',
+      },
+      '&:last-child': {
+        textAlign: 'left',
+      },
+    },
+  },
+}));
 
 Images.propTypes = {
   css: PropTypes.string.isRequired,
@@ -8,15 +28,21 @@ Images.propTypes = {
 };
 
 function Images({ css, url }) {
+  const classes = useStyles();
   return (
     <Grid container spacing={3}>
-      <Grid item xs={12} sm={6}>
-        {/* eslint-disable-next-line */}
-        <img src={url} alt='Original image without filters' />
-      </Grid>
-      <Grid item xs={12} sm={6}>
+      <Grid className={classes.wrapper} item xs={12} sm={6}>
         {/* eslint-disable-next-line */}
         <img
+          className={classes.img}
+          src={url}
+          alt='Original image without filters'
+        />
+      </Grid>
+      <Grid className={classes.wrapper} item xs={12} sm={6}>
+        {/* eslint-disable-next-line */}
+        <img
+          className={classes.img}
           src={url}
           alt='Image with applied filters'
           style={{ filter: css }}
